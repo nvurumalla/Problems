7beebc2468fefb6c9aa8f7e7bfb0a0f9e5d26c04
nvurumalla <nvurumalla@mobileiron.com>
2016-08-18 19:35:56 -0700

    Added Max sum and product

--- src/arrays/RotatedSortedArray.java
+++ src/arrays/RotatedSortedArray.java
@@ -6,29 +6,38 @@ public class RotatedSortedArray {
 		// TODO Auto-generated method stub
 
 		int[] arr = {6,9,10,11,1,2};
-		find(arr, 5);
+		int index = find(arr, 11);
+		System.out.println(index);
 
 	}
 
-	private static int find(int[] arr, int el) {
-		int h = arr.length-1;
+	private static int find(int[] a, int el) {
+		int h = a.length-1;
 		int l = 0;
 		int m = (l+h)/2;
+		int index = -1;
 
 		while (l<=h) {
-			if (arr[m] < arr[m+1] && arr[m] > arr[m-1]) {
-				if (el > arr[m]) {
-					l = m-1;
-					continue;
-				} else if (el > arr[m]) {
-					h = m+1;
-				} else {
-					return m;
+			if (el < a[m]) {
+				if (a[m] > a[h]) {
+					if (el <= a[h]) {
+						l = m+1;
+					} else {
+						h = m-1;
+					}
+				} else if (a[m] < a[h]) {
+					h = m-1;
 				}
-			} else if (arr[m] < arr[m-1]) {
-				return arr[m];
+			} else if (el > a[m] && a[m] > a[l]) {
+				l = m+1;
+			} else if (el > a[m] && a[m] < a[l]) {
+				l = m+1;
+			} else if (el == a[m]){
+				index = m;
+				break;
 			}
+			m = (l+h)/2;
 		}
-		return -1;
+		return index;
 	}
 }
